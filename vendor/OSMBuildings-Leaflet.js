f7bc6d7ce50ce18eaeccebeab43029f073ed0c83
shumaojie <yanjie.jw@163.com>
2026-05-24 15:44:16 +0800

    comment

---
+++ vendor/OSMBuildings-Leaflet.js
@@ -0,0 +1,5 @@
+// OSM Buildings Leaflet plugin loader placeholder
+// 实际部署建议用CDN或本地完整包，这里用动态 script 加载
+// https://osmbuildings.org/documentation/viewer/
+// https://cdn.osmbuildings.org/OSMBuildings-Leaflet.js
+// https://cdn.osmbuildings.org/OSMBuildings.js
---
+++ vendor/OSMBuildings.js
@@ -0,0 +1,2 @@
+// 真实部署请用官方最新版 https://osmbuildings.org/documentation/viewer/
+// 这里仅作本地引用占位，实际内容需用 curl 或浏览器下载 https://cdn.osmbuildings.org/OSMBuildings.js
